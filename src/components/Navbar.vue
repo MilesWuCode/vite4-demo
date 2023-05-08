9d0e1aabf7d02d575fc036f209cdb34abbd179bc
Miles Wu <slion0725@gmail.com>
2023-05-09 00:44:17 +0800

    upd

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,28 +15,49 @@ const emit = defineEmits<{
 
 <template>
   <div class="sticky top-0 bg-white border-b opacity-1 navbar border-gray">
+    <!-- left:menu -->
     <div class="flex-none">
       <button
         type="button"
-        class="md:hidden btn btn-square btn-ghost"
+        class="md:hidden btn btn-circle btn-ghost"
         @click="emit('update:show-offcanvas', !showOffcanvas)"
       >
         <MenuIcon />
       </button>
       <button
         type="button"
-        class="hidden md:flex btn btn-square btn-ghost"
+        class="hidden md:flex btn btn-circle btn-ghost"
         @click="emit('update:show-sidebar', !showSidebar)"
       >
         <MenuIcon />
       </button>
     </div>
+
+    <!-- logo -->
     <div class="flex-1">
       <h3 class="text-xl">Logo</h3>
     </div>
-    <div class="flex-none space-x-2">
+
+    <!-- right:tools -->
+    <div class="flex-none">
+      <button class="btn btn-circle btn-ghost">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-5 h-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+      </button>
       <div class="dropdown dropdown-end">
-        <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+        <label tabindex="0" class="btn btn-circle btn-ghost avatar">
           <div class="w-10 rounded-full">
             <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
           </div>
@@ -55,42 +76,8 @@ const emit = defineEmits<{
           <li><a>Logout</a></li>
         </ul>
       </div>
-      <button class="btn btn-ghost btn-circle">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="w-5 h-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
-      </button>
-      <button class="btn btn-ghost btn-circle">
-        <div class="indicator">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="w-5 h-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-            />
-          </svg>
-          <span class="badge badge-xs badge-primary indicator-item"></span>
-        </div>
-      </button>
-      <button class="btn btn-square btn-ghost">
+
+      <button class="btn btn-circle btn-ghost">
         <MenuKebabIcon />
       </button>
     </div>
