1cd4ff3f7d0f1bbefec87c91a4a1b7d21834e982
Miles Wu <slion0725@gmail.com>
2023-05-19 17:50:24 +0800

    upd

---
+++ src/components/AvatarDropdownMenu.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import Cookies from 'js-cookie'
+import { ref } from 'vue'
+const isLogin = ref(!!Cookies.get('token'))
+</script>
+
+<template>
+  <div v-if="isLogin" class="dropdown dropdown-end">
+    <label tabindex="0" class="btn btn-circle btn-ghost avatar">
+      <div class="rounded-full">
+        <img src="/cat01.jpg" />
+      </div>
+    </label>
+    <ul
+      tabindex="0"
+      class="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+    >
+      <li>
+        <a class="justify-between">
+          Profile
+          <span class="badge">New</span>
+        </a>
+      </li>
+      <li><a>Settings</a></li>
+      <li><button type="button">Logout</button></li>
+    </ul>
+  </div>
+</template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import MenuIcon from '@/components/icons/MenuIcon.vue'
 import MenuKebabIcon from '@/components/icons/MenuKebabIcon.vue'
+import AvatarDropdownMenuVue from '@/components/AvatarDropdownMenu.vue'
 
 defineProps<{
   showOffcanvas: boolean
@@ -60,26 +61,7 @@ const emit = defineEmits<{
       </button>
 
       <!-- avatar,menu -->
-      <div class="dropdown dropdown-end">
-        <label tabindex="0" class="btn btn-circle btn-ghost avatar">
-          <div class="rounded-full">
-            <img src="/cat01.jpg" />
-          </div>
-        </label>
-        <ul
-          tabindex="0"
-          class="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-        >
-          <li>
-            <a class="justify-between">
-              Profile
-              <span class="badge">New</span>
-            </a>
-          </li>
-          <li><a>Settings</a></li>
-          <li><button type="button">Logout</button></li>
-        </ul>
-      </div>
+      <AvatarDropdownMenuVue />
 
       <button class="btn btn-circle btn-ghost">
         <MenuKebabIcon />
