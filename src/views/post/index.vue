401badd60030aff9a096c4ad33e415a986bef407
Miles Wu <slion0725@gmail.com>
2023-06-13 15:02:38 +0800

    upd

--- src/views/post/index.vue
+++ src/views/post/index.vue
@@ -63,15 +63,17 @@ const onSlideChange = (e: any) => {
     class="h-96"
   >
     <swiper-slide v-for="post in data?.data" :key="post.id" class="max-w-sm">
-      <div class="shadow-xl card card-compact bg-base-100">
-        <figure>
-          <img :src="post.cover" :alt="post.title" class="object-cover w-full h-48" />
-        </figure>
-        <div class="card-body">
-          <h2 class="truncate card-title">{{ `${post.id} - ${post.title}` }}</h2>
-          <p class="h-20 line-clamp-4">{{ post.content }}</p>
+      <RouterLink :to="`/post/${post.id}`">
+        <div class="shadow-xl card card-compact bg-base-100">
+          <figure>
+            <img :src="post.cover" :alt="post.title" class="object-cover w-full h-48" />
+          </figure>
+          <div class="card-body">
+            <h2 class="truncate card-title">{{ `${post.id} - ${post.title}` }}</h2>
+            <p class="h-20 line-clamp-4">{{ post.content }}</p>
+          </div>
         </div>
-      </div>
+      </RouterLink>
     </swiper-slide>
   </swiper-container>
 </template>
