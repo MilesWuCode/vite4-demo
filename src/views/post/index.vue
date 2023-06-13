42f3a5db171403dae8d7ddc9f1de9482710289ed
Miles Wu <slion0725@gmail.com>
2023-06-13 14:57:42 +0800

    upd

--- src/views/post/index.vue
+++ src/views/post/index.vue
@@ -2,7 +2,38 @@
 // @ts-ignore
 import { register } from 'swiper/element/bundle'
 
+import { useQuery } from '@tanstack/vue-query'
+import axios from '@/utils/axios'
+import { get } from 'lodash-es'
+
 register()
+
+type Post = {
+  id: string
+  title: string
+  content: string
+  cover: string
+}
+
+type Posts = {
+  data: Post[]
+  meta: {
+    currentPage: number
+    perPage: number
+    lastPage: number
+  }
+}
+
+const fetchPosts = () => {
+  return axios.get('/api/post', { params: { page: 1, limit: 20 } }).then(({ data }) => {
+    return data
+  })
+}
+
+const { isLoading, isError, data, error } = useQuery<Posts, Error>({
+  queryKey: ['posts'],
+  queryFn: fetchPosts
+})
 
 const onProgress = (e: any) => {
   const [progress] = e.detail
@@ -17,35 +48,30 @@ const onSlideChange = (e: any) => {
 
 <template>
   <h1 class="text-3xl">Post</h1>
+
   <swiper-container
-    :slides-per-view="1"
-    :space-between="10"
+    v-if="data"
+    slides-per-view="auto"
+    :space-between="14"
     :centered-slides="true"
     :pagination="{
       hideOnClick: true
     }"
-    :breakpoints="{
-      768: {
-        slidesPerView: 3
-      }
-    }"
     :loop="true"
     @progress="onProgress"
     @slidechange="onSlideChange"
+    class="h-96"
   >
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide>
-    <swiper-slide>Slide 3</swiper-slide>
-    <swiper-slide>Slide 4</swiper-slide>
-    <swiper-slide>Slide 5</swiper-slide>
-    <swiper-slide>Slide 6</swiper-slide>
-    <swiper-slide>Slide 7</swiper-slide>
+    <swiper-slide v-for="post in data?.data" :key="post.id" class="max-w-sm">
+      <div class="shadow-xl card card-compact bg-base-100">
+        <figure>
+          <img :src="post.cover" :alt="post.title" class="object-cover w-full h-48" />
+        </figure>
+        <div class="card-body">
+          <h2 class="truncate card-title">{{ post.id }} - {{ post.title }}</h2>
+          <p class="h-20 line-clamp-4">{{ post.content }}</p>
+        </div>
+      </div>
+    </swiper-slide>
   </swiper-container>
 </template>
-
-<style scoped>
-swiper-slide {
-  border: 1px solid red;
-  height: 200px;
-}
-</style>
