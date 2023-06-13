56b4c0983d597aa5ddcccf32c5fb4feed6bc5c3c
Miles Wu <slion0725@gmail.com>
2023-06-13 14:58:50 +0800

    upd

--- src/views/post/index.vue
+++ src/views/post/index.vue
@@ -68,7 +68,7 @@ const onSlideChange = (e: any) => {
           <img :src="post.cover" :alt="post.title" class="object-cover w-full h-48" />
         </figure>
         <div class="card-body">
-          <h2 class="truncate card-title">{{ post.id }} - {{ post.title }}</h2>
+          <h2 class="truncate card-title">{{ `${post.id} - ${post.title}` }}</h2>
           <p class="h-20 line-clamp-4">{{ post.content }}</p>
         </div>
       </div>
