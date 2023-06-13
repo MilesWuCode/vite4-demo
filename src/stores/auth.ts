b71f10209321fdb95cf719400ff2f8d9a6deaba9
Miles Wu <slion0725@gmail.com>
2023-06-13 16:52:05 +0800

    upd

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -22,38 +22,28 @@ export const useAuthStore = defineStore('auth', () => {
     const state = !!Cookies.get('token')
 
     /**
-     * 若和先前isLogin不同表示登入或登出
+     * 若和先前isLogin相同
+     * 沒變動離開
      */
     if (state === isLogin.value) {
-      // 沒變動,離開
       return
-    } else {
-      // 有變動更新isLogin
-      isLogin.value = state
     }
+
+    /**
+     * 有變動更新狀態
+     */
+    isLogin.value = state
 
     /**
      * 登入中
-     * 若用戶沒有資料
      * 更新用戶資料
-     */
-    if (state && user.value === null) {
-      // 有token但沒用戶資料時取得資料
-      await fetchUser().catch(() => {
-        // 發生錯誤時登出
-        Cookies.remove('token')
-
-        isLogin.value = false
-
-        user.value = null
-      })
-    }
-
-    /**
+     *
      * 未登入
      * 清空用戶資料
      */
-    if (!state) {
+    if (state) {
+      await fetchUser()
+    } else {
       user.value = null
     }
   }
@@ -76,15 +66,25 @@ export const useAuthStore = defineStore('auth', () => {
     const state = !!Cookies.get('token')
 
     if (state) {
-      await axios.get('/api/me').then(({ data }) => {
-        user.value = {
-          id: get(data, 'data.id', ''),
-          name: get(data, 'data.name', ''),
-          email: get(data, 'data.email', ''),
-          avatar: get(data, 'data.avatar', ''),
-          email_verified_at: get(data, 'data.email_verified_at')
-        }
-      })
+      await axios
+        .get('/api/me')
+        .then(({ data }) => {
+          user.value = {
+            id: get(data, 'data.id', ''),
+            name: get(data, 'data.name', ''),
+            email: get(data, 'data.email', ''),
+            avatar: get(data, 'data.avatar', ''),
+            email_verified_at: get(data, 'data.email_verified_at')
+          }
+        })
+        .catch(() => {
+          // 發生錯誤時登出
+          Cookies.remove('token')
+
+          isLogin.value = false
+
+          user.value = null
+        })
     }
   }
 
