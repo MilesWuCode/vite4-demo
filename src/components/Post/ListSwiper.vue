ead766b54d2e955aa477b320ea4fbdc854d72d85
Miles Wu <slion0725@gmail.com>
2023-07-02 23:36:22 +0800

    upd

--- src/components/Post/ListSwiper.vue
+++ src/components/Post/ListSwiper.vue
@@ -4,6 +4,7 @@ import PostCard from '@/components/Post/Card.vue'
 
 // @ts-ignore
 import { register } from 'swiper/element/bundle'
+import { ref } from 'vue'
 
 register()
 
@@ -20,27 +21,43 @@ const onProgress = (e: any) => {
 const onSlideChange = (e: any) => {
   console.log('slide changed', e)
 }
+const swiperEl = ref(null)
+const prev = () => {
+  swiperEl.value && swiperEl.value.swiper.slidePrev()
+}
+const next = () => {
+  swiperEl.value && swiperEl.value.swiper.slideNext()
+}
 </script>
 
 <template>
-  <swiper-container
-    slides-per-view="auto"
-    :autoplay-delay="3000"
-    :centered-slides="false"
-    :free-mode="true"
-    :keyboard="true"
-    :rewind="true"
-    :mousewheel="false"
-    :navigation="true"
-    :pagination-clickable="true"
-    :pagination="true"
-    :space-between="16"
-    @progress="onProgress"
-    @slidechange="onSlideChange"
-    class="h-96"
-  >
-    <swiper-slide v-for="post in posts.data" :key="post.id" class="max-w-sm">
-      <PostCard :post="post" />
-    </swiper-slide>
-  </swiper-container>
+  <div class="relative">
+    <button type="button" class="btn-circle btn absolute left-4 top-[40%] z-10" @click="prev">
+      ❮
+    </button>
+    <button type="button" class="btn-circle btn absolute right-4 top-[40%] z-10" @click="next">
+      ❯
+    </button>
+    <swiper-container
+      ref="swiperEl"
+      slides-per-view="auto"
+      :autoplay-delay="3000"
+      :centered-slides="false"
+      :free-mode="true"
+      :keyboard="true"
+      :rewind="true"
+      :mousewheel="false"
+      :navigation="false"
+      :pagination-clickable="true"
+      :pagination="true"
+      :space-between="16"
+      @progress="onProgress"
+      @slidechange="onSlideChange"
+      class="h-96"
+    >
+      <swiper-slide v-for="post in posts.data" :key="post.id" class="max-w-sm">
+        <PostCard :post="post" />
+      </swiper-slide>
+    </swiper-container>
+  </div>
 </template>
