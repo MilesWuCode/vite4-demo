cca02d96909145f8b231cd0c7dff364c1404cf18
Miles Wu <slion0725@gmail.com>
2023-07-03 16:00:51 +0800

    upd

--- src/components/Post/MyPosts.vue
+++ src/components/Post/MyPosts.vue
@@ -2,47 +2,13 @@
 import { useQuery } from '@tanstack/vue-query'
 import axios from '@/utils/axios'
 import localforage from 'localforage'
-import MyPosts from '@/components/Post/MyPosts.vue'
-import PostListSwiper from '@/components/Post/ListSwiper.vue'
-
-export type Post = {
-  id: string
-  title: string
-  content: string
-  cover: string
-  reaction: Reaction
-  user: {
-    id: string
-    name: string
-    avatar: string
-  }
-}
-
-export type Posts = {
-  cached_at: Date
-  data: Post[]
-  meta: {
-    currentPage: number
-    perPage: number
-    lastPage: number
-  }
-}
-
-type Reaction = {
-  like_count: number
-  dislike_count: number
-  like_state: 'like' | 'dislike' | ''
-  favorite_state: boolean
-}
-
-export type FavoriteCatch = {
-  state: boolean
-  time: Date
-}
+import PostCard from '@/components/Post/Card.vue'
+import PostCreateDialog from '@/components/Post/CreateDialog.vue'
+import type { Posts, FavoriteCatch } from '@/views/post/index.vue'
 
 const fetchData = () => {
   return axios
-    .get('/api/post', { params: { page: 1, limit: 6 } })
+    .get('/api/post', { params: { page: 1, limit: 4 } })
     .then(({ data }: { data: Posts }) => {
       // api建立時間或快取時間
       const catchTime = new Date(data.cached_at)
@@ -69,28 +35,16 @@ const fetchData = () => {
 }
 
 const { isLoading, isError, data, error } = useQuery<Posts, Error>({
-  queryKey: ['posts'],
+  queryKey: ['my-posts'],
   queryFn: fetchData
 })
 </script>
 
 <template>
-  <!-- title -->
-  <h1 class="text-3xl">Post</h1>
+  <div class="space-y-2">
+    <!-- create -->
+    <PostCreateDialog />
 
-  <!-- breadcrumbs -->
-  <div class="breadcrumbs text-sm">
-    <ul>
-      <li>Long text 1</li>
-      <li>Long text 2</li>
-      <li>Long text 3</li>
-      <li>Long text 4</li>
-      <li>Long text 5</li>
-    </ul>
-  </div>
-
-  <h2 class="text-2xl">New Post</h2>
-  <div>
     <!-- loading -->
     <div v-if="isLoading" class="flex items-center justify-center">
       <span class="loading loading-spinner loading-md"></span>
@@ -101,11 +55,19 @@ const { isLoading, isError, data, error } = useQuery<Posts, Error>({
       {{ error?.message }}
     </div>
 
-    <!-- swiper -->
-    <PostListSwiper v-else-if="data" :posts="data" />
+    <!-- list -->
+    <div v-if="data" class="flex flex-wrap justify-center gap-2 md:justify-start">
+      <PostCard v-for="post in data.data" :key="post.id" :post="post" class="w-[24rem] max-w-sm" />
+    </div>
+
+    <!-- Pagination -->
+    <div class="flex justify-center">
+      <div class="join">
+        <button class="join-item btn">1</button>
+        <button class="btn-active join-item btn">2</button>
+        <button class="join-item btn">3</button>
+        <button class="join-item btn">4</button>
+      </div>
+    </div>
   </div>
-
-  <h2 class="text-2xl">My Posts</h2>
-
-  <MyPosts class="md:m-2" />
 </template>
