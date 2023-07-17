089751285773ea89d2ef80566ae9b5244e2153af
Miles Wu <slion0725@gmail.com>
2023-07-17 21:49:02 +0800

    upd

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
-import App from './App.vue'
-import router from './router'
-import { createApp } from 'vue'
-import { createHead } from '@unhead/vue'
-import { createPinia } from 'pinia'
-import { VueQueryPlugin } from '@tanstack/vue-query'
-import echoInit from '@/utils/laravel-echo'
-
 import './assets/main.css'
 import './assets/nprogress.css'
 
-// 初始化echo
+import App from './App.vue'
+import router from './router'
+
+import { createApp } from 'vue'
+import { createHead } from '@unhead/vue'
+import { createPinia } from 'pinia'
+
+import { VueQueryPlugin } from '@tanstack/vue-query'
+import echoInit from '@/utils/laravel-echo'
+
 echoInit()
 
 const app = createApp(App)
