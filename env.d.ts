439418a197571797a7cb1ab81bb018569810c294
Miles Wu <slion0725@gmail.com>
2023-09-14 14:45:40 +0800

    upd

--- env.d.ts
+++ env.d.ts
@@ -5,8 +5,14 @@
 /// <reference types="vite-plugin-vue-layouts/client.d.ts" />
 
 interface ImportMetaEnv {
+  // base
   readonly VITE_APP_NAME: string
   readonly VITE_API_URL: string
+  // pusher
+  readonly VITE_PUSHER_APP_KEY: string
+  readonly VITE_PUSHER_HOST: number
+  readonly VITE_PUSHER_PORT: string
+  readonly VITE_PUSHER_APP_CLUSTER: string
   // more env variables...
 }
 
